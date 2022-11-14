61280d54e575ced936e21ad84af41f7934b5b51c
ArunKrishnan0x168 <arun0x168codes@gmail.com>
2022-11-14 20:15:48 +0530

    Added files for task-10
    Task 10

---
+++ task-10/pset/week 1/mario.c
@@ -0,0 +1,30 @@
+//Author : Arun Krishnan (AM.EN.U4CSE22004)
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    do
+    {
+        n = get_int("Height: ");
+    } while (n<1 || n >8);
+
+    for (int i =0;i<n;i++)
+    {
+        for (int j =0;j<n;j++)
+        {
+            if (i+j < n-1)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("#");
+            }
+        }
+        printf("\n");
+        
+    }
+    
+}
