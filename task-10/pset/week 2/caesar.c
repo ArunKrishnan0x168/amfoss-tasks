dab0e072f88d298c9604cd0ec983209e87f6ae14
ArunKrishnan0x168 <arun0x168codes@gmail.com>
2022-11-14 22:13:00 +0530

    Added files for task-10
    Task 10

---
+++ task-10/pset/week 2/caesar.c
@@ -0,0 +1,43 @@
+///Author : Arun Krishnan (AM.EN.U4CSE22004)
+
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int main(int argc,string argv[])
+{
+    if(argc != 2)
+    {
+        printf("Use ./caesar");
+        return 1;
+    }
+    for (int k =0; k < strlen(argv[1]);k++)
+    {
+        if (isalpha(argv[1][k]))
+        {
+            printf("Use ./caesar");
+            return 1;
+        }
+    }
+    int k = atoi(argv[1]) % 26;
+    string plaintext = get_string("PlainText: ");
+    printf("ciphertext: ");
+    for(int i=0;i<strlen(plaintext);i++)
+    {
+        if(!isalpha(plaintext[i]))
+        {
+            printf("%c",plaintext[i]);
+            continue;
+        }
+        int ascii_offset = isupper(plaintext[i]) ? 65 : 97;
+        int pi = plaintext[i] - ascii_offset;
+        int ci = (pi+k)%26;
+
+        printf("%c",ci+ascii_offset);
+    }
+    printf("\n");
+    return 0;
+}
