49a732209a361f1e9af3a9782a58c51985ef676a
ArunKrishnan0x168 <arun0x168codes@gmail.com>
2022-11-18 23:05:18 +0530

    Added files for task-15
    Task 15

---
+++ task-15/euler05.py
@@ -0,0 +1,21 @@
+#Author : Arun Krishnan(AM.EN.U4CSE22004)
+
+#Such a number would be known as the lcm
+
+import math
+
+
+def lcm(n):
+    a = 1
+    for i in range(1,n+1):
+        a = (a*i)//math.gcd(i,a)
+
+    return a
+
+T = int(input())
+
+for i in range(T):
+    N = int(input())
+    result = lcm(N)
+    print(result)
+    
